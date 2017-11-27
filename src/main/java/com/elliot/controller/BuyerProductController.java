049007a6240e53e54f2c93c8a95de1ed5ef93ec5
b2stry <liupei521@live.com>
2017-11-27 22:30:11 +0800

    init

--- src/main/java/com/elliot/controller/BuyerProductController.java
+++ src/main/java/com/elliot/controller/BuyerProductController.java
@@ -18,9 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/**
- * Created by Elliot on 2017/9/20.
- */
+
 @RestController
 @RequestMapping("/buyer/product")
 public class BuyerProductController {
